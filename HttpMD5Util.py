5475c9c2a33b21455b19789e969f5d8b357625c6
LaoLiuLaoliu <miraclecome@gmail.com>
2021-02-17 15:26:06 +0800

    a

---
+++ HttpMD5Util.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#用于进行http请求，以及MD5加密，生成签名的工具类
+
+import http.client
+import requests
+import urllib
+import json
+import hashlib
+import time
+
+class HttpMD5Util(object):
+    def __init__(self):
+        self.session = requests.sessions.Session()
+
+
+    def httpGet(self, url, resource, params=''):
+        resp = self.session.request('GET', url, params, timeout=10)
+        print(resp)
+        data = resp._content
+        print(data)
+        return json.loads(data)
+
+def buildMySign(params, secretKey):
+    sign = ''
+    for key in sorted(params.keys()):
+        sign += key + '=' + str(params[key]) +'&'
+    data = sign+'secret_key='+secretKey
+    return  hashlib.md5(data.encode("utf8")).hexdigest().upper()
+
+
+def httpPost(url, resource, params):
+     headers = {
+            "Content-type" : "application/x-www-form-urlencoded",
+     }
+     conn = http.client.HTTPSConnection(url, timeout=10)
+     temp_params = urllib.parse.urlencode(params)
+     conn.request("POST", resource, temp_params, headers)
+     response = conn.getresponse()
+     data = response.read().decode('utf-8')
+     params.clear()
+     conn.close()
+     return data
+
---
+++ secret.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Yuande Liu <miraclecome (at) gmail.com>
+
+API_KEY = "a23c42d4-4f83-418f-b1be-d400d1050910"
+SECRET_KEY = "FFBEBEAAA422D33C01ACCC60436CF121"
+IP = "111.19.40.123"
+Label = "everyday"
+Permissions = "Read"
+
+CONTENT_TYPE = 'Content-Type'
+OK_ACCESS_KEY = 'OK-ACCESS-KEY'
+OK_ACCESS_SIGN = 'OK-ACCESS-SIGN'
+OK_ACCESS_TIMESTAMP = 'OK-ACCESS-TIMESTAMP'
+OK_ACCESS_PASSPHRASE = 'OK-ACCESS-PASSPHRASE'
+APPLICATION_JSON = 'application/json'
+BASE_URL = 'https://www.okex.cn'
+# WS_URL = 'ws://echo.websocket.org/'
+WS_URL = 'wss://real.okex.com:8443/ws/v3'
+PASS_PHRASE = ''
