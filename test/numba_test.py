01f914bcb899b24cfe05ec16855c35d80f62558d
LaoLiuLaoliu <miraclecome@gmail.com>
2021-03-06 23:15:02 +0800

    a

--- test/numba_test.py
+++ test/numba_test.py
@@ -7,21 +7,38 @@ import numba
 
 class A(object):
 
-  @staticmethod
-  @numba.jit(nopython=True)
-  def func(h, l, prices):
-    for p in prices:
-      if l < p < h:
-        return True
-    return False
+    @staticmethod
+    @numba.jit(nopython=True)
+    def func(h, l, prices):
+        for p in prices:
+            if l < p < h:
+                return True
+        return False
 
+    @staticmethod
+#    @numba.vectorize('float64(float64, float64, float64)', target='parallel', nopython=True)
+    def rule_with_parallel(y, a, b):
+        if y < a:
+            return a
+        if y > b:
+            return b
+        return y
 
-def run_multi_times()
+def run_multi_times():
     t = time.time()
     for i in range(100):
-      A.func(9.1, 3.2, np.asarray([2.1, 4.2], dtype=np.float32))
-    print(time.time() - t)
+      A.func(9.1, 3.2, np.asarray([2.1, 4.2]))
+    print(time.time() - t)  # 0.1, but python 0.00033
 
+
+def another_run_multi_times():
+    t = time.time()
+    for i in range(100):
+        for j in [2.1, 4.2]:
+            A.rule_with_parallel(j, 9.1, 3.2)
+    print(time.time() - t)  # 0.0154, python 5.22e-05
+
+another_run_multi_times()
 
 def numba_list_cost():
     from numba.typed import List
@@ -39,4 +56,4 @@ def test():
     print(np.asarray(c) is c)  # False
     r = a.func(9.1, 3.2, np.asarray(c, dtype=np.float32))
 
-test()
+# test()
