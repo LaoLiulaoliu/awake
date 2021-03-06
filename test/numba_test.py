3a0cf59abccd69d33cc059ba6baaf087f6fde7be
LaoLiuLaoliu <miraclecome@gmail.com>
2021-03-06 22:33:54 +0800

    a

--- test/numba_test.py
+++ test/numba_test.py
@@ -3,13 +3,12 @@
 
 import time
 import numpy as np
-import numba as nb
-from numba.typed import List
+import numba
 
 class A(object):
 
   @staticmethod
-  @nb.jit(nopython=True)
+  @numba.jit(nopython=True)
   def func(h, l, prices):
     for p in prices:
       if l < p < h:
@@ -17,20 +16,27 @@ class A(object):
     return False
 
 
-t = time.time()
-List([1,2,3])
-print('numba List cost too much time: ', time.time() - t)
+def run_multi_times()
+    t = time.time()
+    for i in range(100):
+      A.func(9.1, 3.2, np.asarray([2.1, 4.2], dtype=np.float32))
+    print(time.time() - t)
 
-t = time.time()
-a = A()
-b = {1:2.1, 3:4.2}
-c = list(b.values())
-print(np.asarray(c, dtype=np.float32) is c)
-print(np.asarray(c) is c)
-r = a.func(9.1, 3.2, np.asarray(c, dtype=np.float32))
 
-# add numba cost more time here
-for i in range(100):
-  a.func(9.1, 3.2, np.asarray(c, dtype=np.float32))
-print(time.time() - t)
+def numba_list_cost():
+    from numba.typed import List
+    t = time.time()
+    List([1,2,3])
+    print('numba List cost too much time: ', time.time() - t)
 
+
+def test():
+    t = time.time()
+    a = A()
+    b = {1:2.1, 3:4.2}
+    c = list(b.values())
+    print(np.asarray(c, dtype=np.float32) is c)  # False
+    print(np.asarray(c) is c)  # False
+    r = a.func(9.1, 3.2, np.asarray(c, dtype=np.float32))
+
+test()
