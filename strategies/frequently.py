d52e7382768661c09b8ebb95edaa280e6b4427d1
laoliu <q_0_0_p@163.com>
2021-03-26 16:04:33 +0800

    a

--- strategies/frequently.py
+++ strategies/frequently.py
@@ -68,9 +68,9 @@ def strategy(state, enobs=3):
             print(f'coin: {coin}, money: {money}')
             print('trend: ', last_time, timestamp, current_price, best_ask, best_bid, best_ask_size, best_bid_size)
             if best_ask - 10**-enobs * 3 >= best_bid:  # e.g best_ask: 7, best_bid: 4, 2 slots between them
-                size = max(best_ask_size, best_bid_size)
+                size = min(best_ask_size, best_bid_size, coin)  # hold coin < market bid coin
                 buy_price = round(best_bid + 10**-enobs, enobs)  # buy before sell
-                size = min(size, coin)  # hold coin < market bid coin
+                print(f'buy_price: {buy_price}, size: {size}')
                 if size > 0 and buy_price < money:
                     sell_price = round(best_ask - 10**-enobs, enobs)
                     order_ids = place_batch_orders([
