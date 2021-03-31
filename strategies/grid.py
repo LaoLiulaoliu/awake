e67f0e909dc3399d867797e54a1d750327402e5c
laoliu <q_0_0_p@163.com>
2021-03-31 14:51:20 +0800

    a

--- strategies/grid.py
+++ strategies/grid.py
@@ -25,61 +25,55 @@ BOARD_LOT = min(round(INIT_COIN / GRID_NUM, effective_number_of_bits), AVERAGE_A
 COIN_UNIT, MONEY_UNIT = list(map(str.upper, INSTRUMENT[VALUTA_IDX].split('-')))
 
 
+def grid_init_orders(state, last_trade_price, enobs):
+    while True:
+        available = state.get_available()
+        coin = available[COIN_UNIT]
+        money = available[MONEY_UNIT]
+
+        buy_price = round(last_trade_price - SPACING_PRICE, enobs)
+        sell_price = round(last_trade_price + SPACING_PRICE, enobs)
+        if coin > BOARD_LOT and buy_price < money:
+            order_ids = place_batch_orders([
+                {'price': buy_price, 'size': BOARD_LOT, 'side': 'buy', 'instrument_id': INSTRUMENT[VALUTA_IDX]},
+                {'price': sell_price, 'size': BOARD_LOT, 'side': 'sell', 'instrument_id': INSTRUMENT[VALUTA_IDX]}
+            ])
+
+            if 0 in order_ids:
+                for i, oid in enumerate(order_ids):
+                    if oid != 0:
+                        cancel_order(oid)
+                        state.delete_canceled_orders([oid])
+                        side = 'buy' if i == 0 else 'sell'
+                        print(f'grid init {side} failed, buy_price: {buy_price}, sell_price: {sell_price}, size: {BOARD_LOT}')
+                gevent.sleep(60)
+                continue
+
+            return int(time.time()), order_ids[0], order_ids[1]
+        gevent.sleep(60)
+
+
 def stop_loss(money_remain, ratio=STOP_LOSS_RATIO):
-    if money_remain < INIT_MONEY * ratio:
-        print(f'money remain: {money_remain}. Send Alarm!!!  Sleep and operate by hand')
-        gevent.sleep(1800)
-        return True
-    return False
+    while True:
+        if money_remain < INIT_MONEY * ratio:
+            print(f'money remain: {money_remain}. Send Alarm!!!  Sleep and operate by hand')
+            gevent.sleep(1800)
+        else:
+            break
 
-def place_init_orders(state, last_trade_price, enobs):
-    available = state.get_available()
-    coin = available[COIN_UNIT]
-    money = available[MONEY_UNIT]
-
-    buy_price = round(last_trade_price - SPACING_PRICE, enobs)
-    sell_price = round(last_trade_price + SPACING_PRICE, enobs)
-    if coin > BOARD_LOT and buy_price < money:
-        order_ids = place_batch_orders([
-            {'price': buy_price, 'size': BOARD_LOT, 'side': 'buy', 'instrument_id': INSTRUMENT[VALUTA_IDX]},
-            {'price': sell_price, 'size': BOARD_LOT, 'side': 'sell', 'instrument_id': INSTRUMENT[VALUTA_IDX]}
-        ])
-
-        if 0 in order_ids:
-            for i, oid in enumerate(order_ids):
-                if oid != 0:
-                    cancel_order(oid)
-                    state.delete_canceled_orders([oid])
-                    side = 'buy' if i == 0 else 'sell'
-                    print(f'{side} failed, buy_price: {buy_price}, sell_price: {sell_price}, size: {BOARD_LOT}')
-            return False
-        return int(time.time()), order_ids[0], order_ids[1]
-    return False
-
-def place_modify_order():
-    pass
 
 def strategy(state, enobs=3):
     last_time, last_trade_price, best_ask, best_bid = state.get_latest_trend()
-    buy_sell_pair = []
-
-    r = place_init_orders(state, last_trade_price, enobs)
-    if r is False:
-        return
-    buy_sell_pair.append(r)
+    buy_sell_pair = grid_init_orders(state, last_trade_price, enobs)
 
     while True:
         available = state.get_available()
         coin = available[COIN_UNIT]
         money = available[MONEY_UNIT]
 
-        if stop_loss(money):
-            continue
-        if len(buy_sell_pair) == 0:
-            return False
+        stop_loss(money)
 
         timestamp, current_price, best_ask, best_bid = state.get_latest_trend()
-
 
         for timestamp, buy_order_id, sell_order_id in buy_sell_pair:
             buy_trade = state.get_order_by_id(buy_order_id)
@@ -89,13 +83,20 @@ def strategy(state, enobs=3):
             buy_price = round(current_price - SPACING_PRICE, enobs)
             sell_price = round(current_price + SPACING_PRICE, enobs)
 
+            # modify failed, hold still, then buy lower sell higher.
+            # buy or sell failed, logic chain breaking,
+            # cancel another trade pair, wait and boot on again.
             if buy_state == 2:
                 if buy_price < money:
                     order_id = place_buy_order(buy_price, BOARD_LOT)
+                    if order_id == 0:
+                        cancel_order(sell_order_id)
                 modify_order(sell_order_id, sell_price, BOARD_LOT)
             elif sell_state == 2:
                 if coin > BOARD_LOT:
                     order_id = place_sell_order(sell_price, BOARD_LOT)
+                    if order_id == 0:
+                        cancel_order(buy_order_id)
                 modify_order(buy_order_id, buy_price, BOARD_LOT)
 
 
