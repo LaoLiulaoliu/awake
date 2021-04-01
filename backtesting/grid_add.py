86a0362b88e174f50df77efbeaff44e5e21a6fee
LaoLiuLaoliu <miraclecome@gmail.com>
2021-04-01 23:04:36 +0800

    a

--- backtesting/grid_add.py
+++ backtesting/grid_add.py
@@ -7,7 +7,7 @@ coin = 50.
 size = 1
 first_price = 0
 last_price = 0
-
+earn = 0
 
 with open('log') as fd:
     for line in fd:
@@ -15,7 +15,8 @@ with open('log') as fd:
             price = float(line.split()[-1])
             if first_price == 0:
                 first_price = price
-                print(f'init money[{money}], coin[{coin}], usd: {coin*first_price + money}')
+                earn = coin * first_price + money
+                print(f'init money[{money}], coin[{coin}], usd: {earn}')
             last_price = price
             if 'buy' in line:
                 money -= price * size
@@ -24,4 +25,8 @@ with open('log') as fd:
                 money += price * size
                 coin -= size
 
-print(f'money[{money}], coin[{coin}], usd: {coin*last_price + money}')
+print(f'money[{money}], coin[{coin}], usd: {coin * last_price + money}')
+print(f'earn: {coin * last_price + money - earn}')
+print('if no deal happen, init usd', 50 * first_price + 100,
+    'finial usd: ', 50 * last_price + 100,
+    'earn: ', 50 * (last_price - first_price))
