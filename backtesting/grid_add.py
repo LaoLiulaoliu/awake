df7ca052e0b636361deea826aeb360ec2b4d0b64
LaoLiuLaoliu <miraclecome@gmail.com>
2021-04-01 22:54:55 +0800

    a

--- backtesting/grid_add.py
+++ backtesting/grid_add.py
@@ -2,19 +2,26 @@
 # -*- coding: utf-8 -*-
 
 
-money = 0.
-coin = 0.
+money = 100.
+coin = 50.
 size = 1
-print(f'init money: {money}, coin: {coin}')
+first_price = 0
+last_price = 0
+
 
 with open('log') as fd:
     for line in fd:
         if 'dealed' in line:
+            price = float(line.split()[-1])
+            if first_price == 0:
+                first_price = price
+                print(f'init money[{money}], coin[{coin}], usd: {coin*first_price + money}')
+            last_price = price
             if 'buy' in line:
-                money -= float(line.split()[-1]) * size
+                money -= price * size
                 coin += size
             elif 'sell' in line:
-                money += float(line.split()[-1]) * size
+                money += price * size
                 coin -= size
 
-print(f'money: {money}, coin: {coin}')
+print(f'money[{money}], coin[{coin}], usd: {coin*last_price + money}')
