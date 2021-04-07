3a1a4c36a37f72f435ec8b9ea75dd4dae1ba5419
LaoLiuLaoliu <miraclecome@gmail.com>
2021-04-07 22:20:14 +0800

    a

---
+++ test/test_unique_price.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Yuande Liu <miraclecome (at) gmail.com>
+
+import time
+from collections import defaultdict
+
+def delete_buy_sell_price_from_recorder(buy_price, buy_prices, sell_price, sell_prices, enobs):
+    buy_key = int(buy_price * 10 ** (enobs - 1))
+    buy_value = int(buy_price * 10 ** enobs)
+    sell_key = int(sell_price * 10 ** (enobs - 1))
+    sell_value = int(sell_price * 10 ** enobs)
+
+    buy_prices[buy_key].remove(buy_value)
+    sell_prices[sell_key].remove(sell_value)
+
+def best_buy_sell_price_duplicate(buy_price, buy_prices, sell_price, sell_prices, enobs):
+    """
+    buy_prices: {186: [1861, 1865, 1868], 191: [1911, 1913]}
+    """
+    buy_key = int(buy_price * 10 ** (enobs - 1))
+    buy_value = int(buy_price * 10 ** enobs)
+    sell_key = int(sell_price * 10 ** (enobs - 1))
+    sell_value = int(sell_price * 10 ** enobs)
+
+    if buy_value in buy_prices[buy_key] or sell_value in sell_prices[sell_key]:
+        return True
+    else:
+        buy_prices[buy_key].add(buy_value)
+        sell_prices[sell_key].add(sell_value)
+        return False
+
+
+def main(enobs=3):
+    buy_prices = defaultdict(set)
+    sell_prices = defaultdict(set)
+    buy_sell_pair = defaultdict(tuple)
+
+    prices = [1.732, 1.734, 1.735, 1.736, 1.732, 1.734, 1.735]
+    for p in prices:
+        remove_pair = []
+
+        buy_price = round(p - 2 * 10 ** -enobs, enobs)
+        sell_price = round(p + 2 * 10 ** -enobs, enobs)
+
+        if best_buy_sell_price_duplicate(buy_price, buy_prices, sell_price, sell_prices, enobs):
+            print('continue: ', buy_price, buy_prices, sell_price, sell_prices)
+            continue
+        else:
+            print('add: ', buy_price, buy_prices, sell_price, sell_prices)
+            buy_sell_pair[int(time.time())] = (buy_price, sell_price)
+            time.sleep(1)
+
+            # after filled or timeout
+            for k, v in buy_sell_pair.items():
+                remove_pair.append(k)
+                break
+
+            for i in remove_pair:
+                buy_price, sell_price = buy_sell_pair.pop(i)
+                delete_buy_sell_price_from_recorder(buy_price, buy_prices, sell_price, sell_prices, enobs)
+
+        print('del: ', buy_price, buy_prices, sell_price, sell_prices)
+
+main()
