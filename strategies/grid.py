25b3d672706e4f205216c8388bfd45a873b9553d
LaoLiuLaoliu <miraclecome@gmail.com>
2021-04-11 13:25:16 +0800

    a

--- strategies/grid.py
+++ strategies/grid.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# demand: 1. web server for stop; 2. backtesting for more profit
+# demand: 1. web server for stop
 
 import logging
 import gevent
